5be57cf7b77e9c3654d2ee7d96f809f609b01857
YujeongLEE <dldbwjd24@ewhain.net>
2023-03-28 22:30:50 +0900

    feat : main 화면 카드 뷰

--- src/pages/DetailInfoPage.tsx
+++ src/pages/DetailInfoPage.tsx
@@ -1,9 +1,9 @@
-import StockDetailInfo from "../types/StockDetailInfo";
 import "./styles.css";
 import styled from 'styled-components';
 
 import MarketStatusBox from '../components/MarketStatusBox';
 import TypeTagBox from "../components/TypeTagBox";
+import StockDetailInfo from '../types/StockDetailTypes';
 
 type Props = {
     height : number,
@@ -16,15 +16,42 @@ export default function DetailInfoPage({width, height}:Props) {
         <div className="container">
             <div className="top-area">
                 <div className="stock-title">
-                    <TitleText>{stockInfo.stockTitle}</TitleText>
-                    <MarketStatusBox isOpen={stockInfo.isOpen}></MarketStatusBox>
-                    <div style={{paddingLeft: 15, paddingTop:8,}}>
-                        <TypeTagBox text={stockInfo.stockType}/>
-                        <TypeTagBox text={stockInfo.marketTitle}/>
-                    </div>
+                    {/*특정 div 내로 좁아지는 패딩을 넣고 싶을 땐 내부 div를 만들어서 넣어야함*/}
+                        <TitleText>{stockInfo.stockTitle}</TitleText>
+                        <MarketStatusBox isOpen={stockInfo.tagInfo.open}></MarketStatusBox>
+                        <div style={{backgroundColor: "red", paddingTop:3}}>
+                            <TypeTagBox text={stockInfo.tagInfo.type}/>
+                            <TypeTagBox text={stockInfo.tagInfo.market}/>
+                        </div>
                 </div>
-                <div>
-                    {/*<ViewCard/>*/}
+                {/*카드 뷰를 둘러싼 padding*/}
+                <div className="card-view">
+                        <ViewCard>
+                        {/*카드 뷰 내부의 패딩*/}
+                            <div style={{padding:15}}>
+                                <DefaultText>{"오늘 주가"}</DefaultText>
+                                <div style={{paddingTop:5}}>
+                                    <MainText>{stockInfo.price}</MainText>
+                                    <DefaultText>{"미소"}</DefaultText>
+                                </div>
+                            <div>
+                                {stockInfo.changePrice >=0 ?
+                                    (
+                                        <div>
+                                            <DefaultText style={{color: "#D06464"}}>{"+"}{stockInfo.changePrice}{"미소"}</DefaultText>
+                                            <DefaultText style={{color: "#D06464"}}>{"(+"}{stockInfo.changePercent}{"%)"}</DefaultText>
+                                        </div>
+                                    ):
+                                    (
+                                        <div>
+                                            <DefaultText style={{color: "#5787DE"}}>{"-"}{stockInfo.changePrice}{"미소"}</DefaultText>
+                                            <DefaultText style={{color: "#5787DE"}}>{"(-"}{stockInfo.changePercent}{"%)"}</DefaultText>
+                                        </div>
+                                    )}
+                            </div>
+                        </div>
+                    </ViewCard>
+
                 </div>
             </div>
             <div className="main-area">
@@ -34,43 +61,69 @@ export default function DetailInfoPage({width, height}:Props) {
 }
 
 
-const stockInfo: StockDetailInfo = {
+
+const stockInfo: StockDetailInfo= {
+    symbol:"AAPL",
     stockTitle: "Apple Inc.",
-    marketPrice: "126.37", //가격
-    currency: "미소", //화폐단위
-    stockType:"EQUITY", //타입
-    marketTitle:"NasdaqGS", //상장된 시장
-    isOpen: false, //장의 상태
-    change: 5.20, //변동가격
+    price: 126.37, //가격
+    changePrice: 5.20, //변동가격
     changePercent: 0.1, //변동 퍼센트
-}
+    tagInfo: {
+        type: "EQUITY",
+        market: "Nasdaq",
+        customPriceConfidence: "HIGH",
+        open: false,
+    },
+};
 
 
 const ViewCard = styled.div`
-  
-  width: 100vw;
-  height: 109px;
-  left: 25px;
-  top: 142px;
-    
+
+  height: 100%;
   /* white */
-    
+
   background: #FFFFFF;
   /* default shadow */
-    
+
   box-shadow: 0px 0px 5px 1px rgba(103, 105, 106, 0.25);
   border-radius: 12px;
 `;
 
+const DefaultText = styled.text`
 
-const TitleText = styled.text`
-  
-    
-    padding: 15px;
-    height: 100vh;
-  
+    //height: 100vh;
+
+    color: #67696A;
+
+    font-size: 1.5vh;
+
+    font-family: Pretendard;
+    font-weight: 400;
+`;
+
+const MainText = styled.text`
+
+
+    //height: 100vh;
+
     font-size: 3.5vh;
-  
+
     font-family: Pretendard;
     font-weight: 700;
 `;
+
+
+
+const TitleText = styled.text`
+
+
+    height: 100vh;
+
+    font-size: 3.5vh;
+
+    font-family: Pretendard;
+    font-weight: 700;
+`;
+
+
+
