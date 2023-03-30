eb3a123df52beadbb1efae6f049a80798fff6356
yujeong <dldbwjd24@gmail.com>
2023-03-30 09:32:08 +0900

    feat : chart분석page

--- src/pages/chart/ChartPage.tsx
+++ src/pages/chart/ChartPage.tsx
@@ -1,18 +1,43 @@
-import react from 'react';
+
+import react, {useState} from 'react';
 import PriceLineChart from "./components/PriceLineChart";
-import React from "react";
+
+import * as React from 'react';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+
 
 export default function ChartPage() {
-    //Tab
-    const [period, setPeriod] = React.useState('1d');
+    const [period, setperiod] = useState('1d');
 
+    const handleChange = (
+        event: React.MouseEvent<HTMLElement>,
+        newperiod: string,
+    ) => {
+        setperiod(newperiod);
+    };
 
 
     return (
-        <div style={{height: '100%'}}>
-            <PriceLineChart period={'1d'}/>
-            <div>
-
+        <div style={{height: "100%", display: 'flex', flexDirection: 'column', overflowY: "hidden"}}>
+            <div style={{height:"90%"}}>
+                <PriceLineChart period={period} />
+            </div>
+            <div style={{height:"10%"}}>
+                <ToggleButtonGroup
+                    color="primary"
+                    value={period}
+                    exclusive
+                    onChange={handleChange}
+                    aria-label="Platform"
+                    fullWidth={true}
+                >
+                    <ToggleButton value="1d">1일</ToggleButton>
+                    <ToggleButton value="5d">1주</ToggleButton>
+                    <ToggleButton value="3mo">3달</ToggleButton>
+                    <ToggleButton value="1y">1년</ToggleButton>
+                    <ToggleButton value="5y">5년</ToggleButton>
+                </ToggleButtonGroup>
             </div>
         </div>
     );
--- src/pages/chart/components/PriceLineChart.tsx
+++ src/pages/chart/components/PriceLineChart.tsx
@@ -1,11 +1,9 @@
-import StockDetailTypes from "../../../types/StockDetailTypes";
+
 import styled, {css} from 'styled-components';
 import React from "react";
-import {useEffect} from "react";
 
-// @ts-ignore
-import {LineChart, CartesianGrid, Legend, Line, Tooltip, XAxis, YAxis, ResponsiveContainer} from "recharts";
-import {now} from "lodash";
+import {LineChart,Line, Tooltip, XAxis, YAxis, ResponsiveContainer} from "recharts";
+
 import StockPriceGraphData from "../../../types/StockPriceGraphData";
 
 interface Props {
@@ -35,72 +33,281 @@ export default function PriceLineChart ({period}:Props){
 
     return (
         <ResponsiveContainer>
-        <LineChart data={stockGraphData.data} margin={{ top: 5, right: 10, left: 10, bottom: 5 }}>
-            <XAxis dataKey="time" hide={true} type="number" domain={[stockGraphData.dateInfo.minDate, stockGraphData.dateInfo.maxDate]}/>
-            <YAxis dataKey="price" type="number" domain={[stockGraphData.priceInfo.minPrice, stockGraphData.priceInfo.maxPrice]} hide={true}/>
-            <Tooltip
-                separator={""}
-                formatter={(value,name,props) => [value,""]}
-                labelFormatter={label => toDate(label, period)}
+            <LineChart data={stockGraphData.data} margin={{ top: 5, right: 10, left: 10, bottom: 5 }}>
+                <XAxis dataKey="time" hide={true} type="number" domain={[stockGraphData.dateInfo.minDate, stockGraphData.dateInfo.maxDate]}/>
+                <YAxis dataKey="price" type="number" domain={[stockGraphData.priceInfo.minPrice, stockGraphData.priceInfo.maxPrice]} hide={true}/>
+                <Tooltip
+                    separator={""}
+                    formatter={(value,name,props) => [value,""]}
+                    labelFormatter={label => toDate(label, period)}
 
 
-            />
-            <Line type="linear" dataKey="price" stroke="#8884d8" dot={false} strokeWidth={2}/>
-        </LineChart>
+                />
+                <Line type="linear" dataKey="price" stroke="#8884d8" dot={false} strokeWidth={2}/>
+            </LineChart>
         </ResponsiveContainer>
     );
 }
 
 
-const TextBox = styled.div`
-  
-
-
-`;
-
-// @ts-ignore
-const TagText = styled.text`
-  
-
-
-
-`;
-
-
 const stockGraphData: StockPriceGraphData = {
 
-    symbol: 'AAPL',
-    period:'1d',
-    dateInfo:{
-        maxDate: 1672929000,
-        minDate: 1672324200,
+    "symbol": "AAPL",
+    "period": "3mo",
+    "dateInfo": {
+        "maxDate": 1680010200,
+        "minDate": 1672324200
     },
-    priceInfo:{
-        maxPrice: 130,
-        minPrice: 125,
+    "priceInfo": {
+        "maxPrice": 161,
+        "minPrice": 126
     },
-    data : [
+    "data": [
         {
             "time": 1672324200,
-            "price": 129.61,
+            "price": 129.61
         },
         {
             "time": 1672410600,
-            "price": 129.93,
+            "price": 129.93
         },
         {
             "time": 1672756200,
-            "price": 125.07,
+            "price": 125.07
         },
         {
             "time": 1672842600,
-            "price": 126.36,
+            "price": 126.36
         },
         {
             "time": 1672929000,
-            "price": 125.02,
+            "price": 125.02
         },
-    ],
+        {
+            "time": 1673015400,
+            "price": 129.62
+        },
+        {
+            "time": 1673274600,
+            "price": 130.15
+        },
+        {
+            "time": 1673361000,
+            "price": 130.73
+        },
+        {
+            "time": 1673447400,
+            "price": 133.49
+        },
+        {
+            "time": 1673533800,
+            "price": 133.41
+        },
+        {
+            "time": 1673620200,
+            "price": 134.76
+        },
+        {
+            "time": 1673965800,
+            "price": 135.94
+        },
+        {
+            "time": 1674052200,
+            "price": 135.21
+        },
+        {
+            "time": 1674138600,
+            "price": 135.27
+        },
+        {
+            "time": 1674225000,
+            "price": 137.87
+        },
+        {
+            "time": 1674484200,
+            "price": 141.11
+        },
+        {
+            "time": 1674570600,
+            "price": 142.53
+        },
+        {
+            "time": 1674657000,
+            "price": 141.86
+        },
+        {
+            "time": 1674743400,
+            "price": 143.96
+        },
+        {
+            "time": 1674829800,
+            "price": 145.93
+        },
+        {
+            "time": 1675089000,
+            "price": 143
+        },
+        {
+            "time": 1675175400,
+            "price": 144.29
+        },
+        {
+            "time": 1675261800,
+            "price": 145.43
+        },
+        {
+            "time": 1675348200,
+            "price": 150.82
+        },
+        {
+            "time": 1675434600,
+            "price": 154.5
+        },
+        {
+            "time": 1675693800,
+            "price": 151.73
+        },
+        {
+            "time": 1675780200,
+            "price": 154.65
+        },
+        {
+            "time": 1675866600,
+            "price": 151.92
+        },
+        {
+            "time": 1675953000,
+            "price": 150.87
+        },
+        {
+            "time": 1676039400,
+            "price": 151.01
+        },
+        {
+            "time": 1676298600,
+            "price": 153.85
+        },
+        {
+            "time": 1676385000,
+            "price": 153.2
+        },
+        {
+            "time": 1676471400,
+            "price": 155.33
+        },
+        {
+            "time": 1676557800,
+            "price": 153.71
+        },
+        {
+            "time": 1676644200,
+            "price": 152.55
+        },
+        {
+            "time": 1676989800,
+            "price": 148.48
+        },
+        {
+            "time": 1677076200,
+            "price": 148.91
+        },
+        {
+            "time": 1677162600,
+            "price": 149.4
+        },
+        {
+            "time": 1677249000,
+            "price": 146.71
+        },
+        {
+            "time": 1677508200,
+            "price": 147.92
+        },
+        {
+            "time": 1677594600,
+            "price": 147.41
+        },
+        {
+            "time": 1677681000,
+            "price": 145.31
+        },
+        {
+            "time": 1677767400,
+            "price": 145.91
+        },
+        {
+            "time": 1677853800,
+            "price": 151.03
+        },
+        {
+            "time": 1678113000,
+            "price": 153.83
+        },
+        {
+            "time": 1678199400,
+            "price": 151.6
+        },
+        {
+            "time": 1678285800,
+            "price": 152.87
+        },
+        {
+            "time": 1678372200,
+            "price": 150.59
+        },
+        {
+            "time": 1678458600,
+            "price": 148.5
+        },
+        {
+            "time": 1678714200,
+            "price": 150.47
+        },
+        {
+            "time": 1678800600,
+            "price": 152.59
+        },
+        {
+            "time": 1678887000,
+            "price": 152.99
+        },
+        {
+            "time": 1678973400,
+            "price": 155.85
+        },
+        {
+            "time": 1679059800,
+            "price": 155
+        },
+        {
+            "time": 1679319000,
+            "price": 157.4
+        },
+        {
+            "time": 1679405400,
+            "price": 159.28
+        },
+        {
+            "time": 1679491800,
+            "price": 157.83
+        },
+        {
+            "time": 1679578200,
+            "price": 158.93
+        },
+        {
+            "time": 1679664600,
+            "price": 160.25
+        },
+        {
+            "time": 1679923800,
+            "price": 158.28
+        },
+        {
+            "time": 1680010200,
+            "price": 157.65
+        }
+    ]
 
 
 
