4037f9af2867066c99226eaa9921b01a6b303ad6
yujeong <dldbwjd24@gmail.com>
2023-03-31 15:47:57 +0900

    feat : main card view 구조변경

--- src/pages/details/components/MainCardView.tsx
+++ src/pages/details/components/MainCardView.tsx
@@ -18,31 +18,36 @@ interface Props {
 
 export default function MainCardView({currency,price,changePrice,changePercent}:Props) {
     return (
-        <Card sx={{ minWidth: 150, maxHeight: '100%' }} style={{  boxShadow: "0px 0px 5px 1px rgba(103, 105, 106, 0.25)", borderRadius: 12,}}>
-            <CardContent sx={{paddingTop:1, paddingBottom:1, paddingLeft:1.5}}>
-                <Typography >
+        <Card sx={{ minWidth: 150, height:'100%'}} style={{  boxShadow: "0px 0px 5px 1px rgba(103, 105, 106, 0.25)", borderRadius: 12,}}>
+            <CardContent sx={{height:'100%', padding:0.5}}>
+                <div style={{height: '15%', backgroundColor:'red', padding: 1, display:"flex", alignItems:'center'}}>
                     <TitleText>{"오늘 주가"}</TitleText>
-                </Typography>
-                <Typography variant="h5" component="div">
-                    <MainText>{price}</MainText>
-                </Typography>
-                <Typography sx={{ mb: 2 }} >
-                    {changePrice>0 ? (
-                        <>
-                            <DefaultText style={{color: "#D06464"}}>{"+"}{changePrice}{currency}{"  "}</DefaultText>
-                            <DefaultText style={{color: "#D06464"}}>{"("}{changePercent}{"%)"}</DefaultText>
-                        </>
-                    )
-                        :
-                    (
-                        <>
-                            <DefaultText style={{color: "#5787DE"}}>{"-"}{changePrice}{currency}{"  "}</DefaultText>
-                            <DefaultText style={{color: "#5787DE"}}>{"("}{changePercent}{"%)"}</DefaultText>
-                        </>
-                    )
-                    }
-
-                </Typography>
+                </div>
+                <div style={{height: '30%', display:"flex", alignItems:'center',backgroundColor:'pink'}}>
+                    <Typography variant="h5" component="div">
+                        <MainText>{price}</MainText>
+                        {changePrice>0 ? (
+                            <>
+                                <DefaultText style={{color: "#D06464"}}>{"+"}{changePrice}{currency}{"  "}</DefaultText>
+                                <DefaultText style={{color: "#D06464"}}>{"("}{changePercent}{"%)"}</DefaultText>
+                            </>
+                        )
+                            :
+                        (
+                            <>
+                                <DefaultText style={{color: "#5787DE"}}>{"-"}{changePrice}{currency}{"  "}</DefaultText>
+                                <DefaultText style={{color: "#5787DE"}}>{"("}{changePercent}{"%)"}</DefaultText>
+                            </>
+                        )
+                        }
+                    </Typography>
+                </div>
+                <div style={{display: "flex", flexDirection: 'row',
+                    justifyContent:'center', alignItems:'center',
+                    padding:7, backgroundColor:'blue', height: '35%'}}>
+                    <CustomBtn/>
+                    <CustomBtn/>
+                </div>
             </CardContent>
         </Card>
     );
@@ -52,7 +57,7 @@ const DefaultText = styled.text`
 
     //height: 100vh;
   
-    font-size: 1rem;
+    font-size: 0.6em;
 
     font-family: Pretendard;
     font-weight: 400;
@@ -65,7 +70,7 @@ const DefaultText = styled.text`
 const MainText = styled.text`
   
 
-    font-size: 1.5rem;
+    font-size: 1em;
 
     font-family: Pretendard;
     font-weight: 800;
@@ -81,4 +86,12 @@ const TitleText = styled.text`
 
     font-family: Pretendard;
     font-weight: 600;
+`;
+
+
+const CustomBtn = styled.div`
+  
+    height: 100%;
+    flex:1;
+    background-color: #D06464;
 `;
